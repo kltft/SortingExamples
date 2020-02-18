920a6e12d17d7416a77cbb1e9a080360e35b4949
Cody Kaltoft <cody.kaltoft@gmail.com>
2020-02-17 19:42:04 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortingAndStrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
